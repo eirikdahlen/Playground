30d3f202224397dfbeaaa3cc4a117dfb3770b996
Eirik Dahlen <dahleneirik@gmail.com>
2020-06-18 19:13:55 +0200

    feat: add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Compendium from './Compendiums/Compendium';
+import Navigation from './Navigation/Navigation';
+import * as ROUTES from '../constants/routes';
 
 const App = () => (
-  <div>
+  <Router>
+    <Navigation />
     <div>
-      I've access to Firebase and render something.
-      <Compendium />
+      <div>
+        <Link to={ROUTES.COMPENDIUMS}>COMPENDIUMS</Link>
+      </div>
+      <Switch>
+        <Route path={ROUTES.COMPENDIUMS} component={Compendium} />
+      </Switch>
     </div>
-  </div>
+  </Router>
 );
 
 export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
+const Navigation = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to={ROUTES.COMPENDIUMS}>COMPENDIUMS</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Navigation;
