df025bfb00080a5198de67c68002cf25099ffd54
Eirik Dahlen <dahleneirik@gmail.com>
2020-07-05 09:50:01 +0200

    feat: create PlayBox

---
+++ src/components/PlayBox/PlayBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayBox = ({ name }) => {
+  return (
+    <div className={'playbox-container'}>
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+export default PlayBox;
